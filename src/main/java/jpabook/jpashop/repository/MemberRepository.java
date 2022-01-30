2f84427460a24383f651737a0d61ff04846d1bd0
Hyungseock-Yun <bagzein@gmail.com>
2022-01-31 05:40:29 +0900

    회원 서비스 및 기능 테스트 로직 작성

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -10,10 +11,11 @@ import java.util.List;
 /* SpringBootApplication - 패키지 및 하위들을 전부 component 스캔하여 spring bean으로 자동 등록함.
  (Repository 내부에 component 설정되어있음) */
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-  @PersistenceContext   // spring bean에 등록된 EntityManager 주입시켜 줌
-  private EntityManager em;
+//  @PersistenceContext   // spring bean에 등록된 EntityManager 주입시켜 줌
+  private final EntityManager em;
 
   public void save(Member member) {
     em.persist(member);
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor    // final로 선언된 필드의 생성자를 생성해줌
+public class MemberService {
+
+  private final MemberRepository memberRepository;
+
+  // 회원 가입
+  public Long join(Member member) {
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+
+    return member.getId();
+  }
+
+  // 중복 회원 검증
+  private void validateDuplicateMember(Member member) {
+    List<Member> findMembers = memberRepository.findByName(member.getName());
+    if (!findMembers.isEmpty()) {
+      throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+  }
+
+  // 회원 전체 조회
+  @Transactional(readOnly = true)   // 읽기에는 가급적 readOnly = true를 쓰는 것이 효율적. 삽입, 갱신에 쓰면 DB에 적용이 안됨.
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  // 회원 1건 조회
+  @Transactional(readOnly = true)
+  public Member findOne(Long memberId) {
+    return memberRepository.findOne(memberId);
+  }
+}
