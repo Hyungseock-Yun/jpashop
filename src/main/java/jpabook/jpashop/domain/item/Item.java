05caa3d46650db603d3dd2d0cb482e2c757d4aa7
Hyungseock-Yun <bagzein@gmail.com>
2022-02-02 21:48:02 +0900

    상품 리포지토리 및 서비스 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,24 @@ public abstract class Item {
 
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+
+  /* 비즈니스 로직. 값의 변경은 별도의 method를 만들어서 사용한다. 그것이 객체지향적이니까.. */
+
+  /**
+   * stock 증가
+   * @param stockQuantity
+   */
+ public void addStock(int stockQuantity) {
+    this.stockQuantity += stockQuantity;
+  }
+
+  /**
+   * stock 감소
+   * @param stockQuantity
+   */
+  public void removeStock(int stockQuantity) {
+   int restStock = this.stockQuantity - stockQuantity;
+   if (restStock < 0) throw new NotEnoughStockException("need more stock");
+   this.stockQuantity = restStock;
+  }
 }
